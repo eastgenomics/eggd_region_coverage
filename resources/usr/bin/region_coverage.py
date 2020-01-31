ab2c8d1b1febdd7dec1350ce959033a20020cae5
addy81 <adriana.tou@gmail.com>
2020-01-31 10:06:35 +0000

    first build

--- resources/usr/bin/region_coverage.py
+++ resources/usr/bin/region_coverage.py
@@ -6,7 +6,7 @@ import os
 import argparse
 pp = pprint.PrettyPrinter(indent=4)
 
-sys.path.append('/software/lib/python2.7/site-packages/pysam-0.7.5-py2.7-linux-x86_64.egg/')
+#sys.path.append('/software/lib/python2.7/site-packages/pysam-0.7.5-py2.7-linux-x86_64.egg/')
 import pysam
 
 BIN_0_MIN  = -10
